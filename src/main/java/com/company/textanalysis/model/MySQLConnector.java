22cd7cac9684f7ba845b0b64a0fc354b106b9fa2
Okhotnik <vadim2000ohota@gmail.com>
2021-10-31 11:03:33 +0200

    Update MySQLConnector.java

--- src/main/java/com/company/textanalysis/model/MySQLConnector.java
+++ src/main/java/com/company/textanalysis/model/MySQLConnector.java
@@ -2,13 +2,16 @@ package com.company.textanalysis.model;
 
 import com.company.textanalysis.ui.MySQLConnecting;
 
+//import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+//import java.util.Properties;
 
 public class MySQLConnector implements MySQLConnecting {
 
     @Override
     public Connection connection() {
+        //Спосіб з використанням патерна Одинака
         try {
             PropertySingleton propertySingleton = PropertySingleton.getInstance();
             return DriverManager.getConnection(propertySingleton.mysqlUrl, propertySingleton.mysqlUser, propertySingleton.mysqlPassword);
@@ -16,5 +19,16 @@ public class MySQLConnector implements MySQLConnecting {
             System.err.println("Error Connected MySQL");
             return null;
         }
+        //Простий спосіб
+//        Properties properties = new Properties();
+//        try {
+//            FileInputStream fileInputStream = new FileInputStream("src/main/resources/configuration.properties");
+//            properties.load(fileInputStream);
+//            return DriverManager.getConnection(properties.getProperty("database.mysqlUrl"), properties.getProperty("database.mysqlUser"), properties.getProperty("database.mysqlPassword"));
+//        } catch (Exception e){
+//            System.err.println("Error Connected MySQL");
+//            return null;
+//        }
     }
 }
+
